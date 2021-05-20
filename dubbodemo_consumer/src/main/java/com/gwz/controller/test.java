c94d977acc471758325694e01e11eede622fdb7f
gwz <gwz31415@163.com>
2021-05-20 10:40:30 +0800

    do git commit

--- dubbodemo_consumer/src/main/java/com/gwz/controller/test.java
+++ dubbodemo_consumer/src/main/java/com/gwz/controller/test.java
@@ -3,5 +3,8 @@ package com.gwz.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("dev");
     }
 }

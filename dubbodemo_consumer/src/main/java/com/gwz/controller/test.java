224a81a37f7cb8307f49b4ddc9edc2f1ff24be91
gwz <gwz31415@163.com>
2021-05-20 10:54:29 +0800

    Merge branch 'dev'
    # Conflicts:
    #	dubbodemo_consumer/src/main/java/com/gwz/controller/test.java

--- dubbodemo_consumer/src/main/java/com/gwz/controller/test.java
+++ dubbodemo_consumer/src/main/java/com/gwz/controller/test.java
@@ -2,12 +2,12 @@ package com.gwz.controller;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("dev");
-        System.out.println("dev");
-        System.out.println("dev");
-        System.out.println("dev");
+        System.out.println("master111");
+        System.out.println("master111");
+        System.out.println("master111");
+        System.out.println("dev1111");
+        System.out.println("dev11111");
+        System.out.println("dev1111");
+
     }
 }
